564b3d274b8ef0ef5186fc8d6e58308694379e56
joyceHong0524 <joyceHong0524@gmail.com>
2019-01-17 11:02:11 +0900

    It worked like this

--- web/db.js
+++ web/db.js
@@ -2,16 +2,16 @@ var mysql = require('mysql');
 
 var pool;
 
-exports.connect = function() {
+exports.connect = function(){
     pool = mysql.createPool({
-        connectionLimit: 100,
-        host     : 'localhost',
-        user     : 'root',
-        password : 'Rr115500..',
+        connectionLimit : 100,
+        host : 'localhost',
+        user :'root',
+        password :'Rr115500..',
         database : 'bestfood'
     });
 }
 
-exports.get = function() {
-  return pool;
+exports.get = function(){
+    return pool;
 }
