f5573b7c37a455a2740c58f4600fa65251bd9415
joyceHong0524 <joyceHong0524@gmail.com>
2019-02-14 14:27:06 +0900

    Working on food register Activity&Fragments

---
+++ android/app/src/main/java/com/example/jung/bestpadthai/BestPadthaiRegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.jung.bestpadthai;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jung.bestpadthai.item.FoodInfoItem;
+import com.example.jung.bestpadthai.item.GeoItem;
+import com.example.jung.bestpadthai.lib.GoLib;
+import com.example.jung.bestpadthai.lib.MyLog;
+
+
+public class BestPadthaiRegisterActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
+    public static FoodInfoItem currentItem = null;
+
+    //set basic information to run BestPadthaiLocationFragment
+    
+}
--- android/app/src/main/java/com/example/jung/bestpadthai/ProfileIconActivity.java
+++ android/app/src/main/java/com/example/jung/bestpadthai/ProfileIconActivity.java
@@ -101,6 +101,126 @@ public class ProfileIconActivity extends AppCompatActivity implements View.OnCli
         profileIconFile = FileLib.getInstance().getProfileIconFile(context,profileIconFilename); // 지정한 이름을 이용한 파일 객체를 반환함. 아직 파일이 들어있는 건 아님.
     }
 
+    public void onClick(View v) {
 
+        setProfileIconFile();
+
+        if (v.getId() == R.id.album ){
+            getImageFromAlbum();
+        }
+        else if (v.getId() == R.id.camera) {
+            getImageFromCamera();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_close, menu);
+        return true;
+    }
+
+    //왼쪽 상단의 android.R.id.home을 선택했을 때와
+    //오른쪽 상단의 닫기 메뉴를 클릭했을 때의 동작을 지정한다.
+    //여기서는 모든 클릭이 액티비티를 종료한다.
+    //메뉴를 처리시 true, 못 했을 시에는 false를 return
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.action_close:
+                finish();
+                break;
+        }
+
+        return true;
+
+    }
+
+    //takes picture using camera app
+    private void getImageFromCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(profileIconFile));
+        startActivityForResult(intent, PICK_FROM_CAMERA);
+
+    }
+
+    //get picture using album app
+    private void getImageFromAlbum() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    //return intent to crop the given image
+    private Intent getCropIntent(Uri inputUri, Uri outputUri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(inputUri,"image/*");
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY",1);
+        intent.putExtra("outputX",200);
+        intent.putExtra("outputY",200);
+        intent.putExtra("scale",true);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
+
+        return intent;
+
+    }
+
+
+    private void cropImageFromCamera() {
+        Uri uri = Uri.fromFile(profileIconFile); //현재 profileIconFile 에서 주소를 가져온다
+        Intent intent = getCropIntent(uri, uri); //input uri, output uri 이런 거 없고 그냥 덮어쓴다 이거임
+        startActivityForResult(intent, CROP_FROM_CAMERA);
+    }
+
+    private void cropImageFromAlbum(Uri inputUri) {
+        Uri outputUri = Uri.fromFile(profileIconFile);
+
+        MyLog.d(TAG, "startPickFromAlbum uri"+ inputUri.toString());
+        Intent intent = getCropIntent(inputUri, outputUri);
+        startActivityForResult(intent,CROP_FROM_ALBUM);
+
+    }
+
+
+
+
+    //IMPORTANT
+    //manages activities called by startActivityForResult()
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        MyLog.d(TAG, "onActivityResult "+ intent);
+
+        if(resultCode != RESULT_OK) return;
+
+        if(requestCode == PICK_FROM_CAMERA) {
+            cropImageFromCamera();
+        } else if (requestCode == CROP_FROM_CAMERA){
+            Picasso.with(this).load(profileIconFile).into(profileIconImage);
+            uploadProfileIcon();
+        } else if (requestCode == PICK_FROM_ALBUM && intent != null) {
+            Uri dataUri = intent.getData();
+            if (dataUri != null){
+                cropImageFromAlbum(dataUri);
+            }
+        } else if (requestCode == CROP_FROM_ALBUM && intent != null) {
+            Picasso.with(this).load(profileIconFile).into(profileIconImage);
+            uploadProfileIcon();
+        }
+
+    }
+
+    //uploads profile image on server
+    private void uploadProfileIcon() {
+        RemoteLib.getInstance().uploadMemberIcon(memberInfoItem.seq,profileIconFile);
+
+        memberInfoItem.memberIconFilename = profileIconFilename + ".png";
+
+    }
 
 }
